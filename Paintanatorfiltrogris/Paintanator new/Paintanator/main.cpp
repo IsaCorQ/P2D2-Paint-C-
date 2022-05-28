138d51159672a7eaf16f8fedd77a0206cd08d44b
IsaCorQ <85208919+IsaCorQ@users.noreply.github.com>
2022-05-28 17:24:31 -0600

    Update main.cpp

--- Paintanatorfiltrogris/Paintanator new/Paintanator/main.cpp
+++ Paintanatorfiltrogris/Paintanator new/Paintanator/main.cpp
@@ -5,7 +5,7 @@
  * Proyecto 2 para la clase de algortmos y estructuras de datos 2
  * Profesor Isaac Ramirez
  * Autores: Isa Córdoba Quesada, Ian Hu Pacheco
- * Carné: 2021067015
+ * Carné: 2021067015, 2021062270
  * Version:5 del proyecto
  * Fecha de revisión de codigo: 28 de marzo 2021 8:30pm*/
 int main(int argc, char *argv[])
